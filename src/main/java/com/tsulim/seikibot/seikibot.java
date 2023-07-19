273637fd3f0afeb3c98cd2eab290e8ba43f19cae
tsulim <limxiujia18@gmail.com>
2023-07-20 00:43:22 +0800

    Added comments.

--- src/main/java/com/tsulim/seikibot/seikibot.java
+++ src/main/java/com/tsulim/seikibot/seikibot.java
@@ -11,23 +11,44 @@ public class seikibot {
 
     private final Dotenv config;
     private final ShardManager shardManager;
+
+    /**
+     * Loads environment variables and builds the bot shard manager.
+     *
+     * @throws InvalidTokenException occurs when bot token is invalid.
+     */
     public seikibot() throws InvalidTokenException {
+        // Load environment variables
         config = Dotenv.configure().load();
         String token = config.get("TOKEN");
 
+        // Build shard manager
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.watching("your every move"));
         shardManager = builder.build();
     }
 
+    /**
+     * Retrieves the bot environment variables.
+     * @return the DotEnv instance for the bot.
+     */
     public Dotenv getConfig() {
         return config;
     }
+
+    /**
+     * Retrieves the bot shard manager.
+     * @return the ShardManager instance for the bot.
+     */
     public ShardManager getShardManager() {
         return shardManager;
     }
 
+    /**
+     * Main method where the bot starts.
+     * @param args
+     */
     public static void main(String[] args) {
         try {
             seikibot bot = new seikibot();
