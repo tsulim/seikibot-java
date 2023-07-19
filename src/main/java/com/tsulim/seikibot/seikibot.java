bb296b6302fbff170f83543b34d6c202e3dc8f2b
tsulim <limxiujia18@gmail.com>
2023-07-19 23:29:28 +0800

    Updated Exception for Invalid Token.

--- src/main/java/com/tsulim/seikibot/seikibot.java
+++ src/main/java/com/tsulim/seikibot/seikibot.java
@@ -3,6 +3,7 @@ package com.tsulim.seikibot;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.exceptions.InvalidTokenException;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
@@ -13,7 +14,7 @@ public class seikibot {
     private final Dotenv config;
     private final ShardManager shardManager;
 
-    public seikibot() throws LoginException {
+    public seikibot() throws InvalidTokenException, LoginException {
         config = Dotenv.configure().load();
         String token = config.get("TOKEN");
 
@@ -33,8 +34,10 @@ public class seikibot {
     public static void main(String[] args) {
         try {
             seikibot bot = new seikibot();
-        } catch (LoginException e) { // Catch not working ??
+        } catch (InvalidTokenException e) {
             System.out.println("ERROR: Provided bot token is invalid!");
+        } catch (LoginException e) {
+            System.out.println("ERROR: Login Exception.");
         }
     }
 }
